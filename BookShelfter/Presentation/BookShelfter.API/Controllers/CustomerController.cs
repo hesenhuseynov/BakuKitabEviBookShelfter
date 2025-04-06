2b38f9db763b1074a8d8e311a635e4d776e686da
hesenhuseynov <153264844+hesenhuseynov@users.noreply.github.com>
2025-04-06 23:09:04 +0400

    Update CustomerController.cs

--- BookShelfter/Presentation/BookShelfter.API/Controllers/CustomerController.cs
+++ BookShelfter/Presentation/BookShelfter.API/Controllers/CustomerController.cs
@@ -5,35 +5,21 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BookShelfter.API.Controllers;
 
-[Route("api/[controller]")]
 [ApiController]
-public class CustomerController:ControllerBase
+[Route("api/[controller]")]
+[Authorize(AuthenticationSchemes = "Bearer")]
+public class CustomerController(IMediator mediator) : ControllerBase
 {
-    private readonly IMediator _mediator;
-
-    public CustomerController(IMediator mediator)
-    {
-        _mediator = mediator;
-    }
+    private readonly IMediator _mediator = mediator;
 
     [HttpGet("{appUserId}")]
-    public async Task<IActionResult> GetCustomerByAppUserId(string appUserId)
+    public async Task<IActionResult> GetCustomerByAppUserId([FromRoute] string appUserId)
     {
         var request = new GetCustomerByAppUserIdQueryRequest { UserId = appUserId };
         var response = await _mediator.Send(request);
 
-        if (response.Success)
-        {
-            return Ok(response);
-        }
-
-        return BadRequest(response);
+        return response.Success
+            ? Ok(response)
+            : BadRequest(new { message = response.Message });
     }
-
-
-
-
-
-
-
 }
