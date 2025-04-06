caac928c92bc5cfb1e1f59b54c3cd9714e0e0279
hesenhuseynov <153264844+hesenhuseynov@users.noreply.github.com>
2025-04-06 23:12:33 +0400

    Update OrdersController.cs

--- BookShelfter/Presentation/BookShelfter.API/Controllers/OrdersController.cs
+++ BookShelfter/Presentation/BookShelfter.API/Controllers/OrdersController.cs
@@ -1,115 +1,83 @@
-﻿using BookShelfter.Application.Features.Commands.Order.CreateOrder;
+using BookShelfter.Application.Features.Commands.Order.CreateOrder;
 using BookShelfter.Application.Features.Commands.Order.DeleteOrder;
 using BookShelfter.Application.Features.Commands.Order.UpdateOrder;
 using BookShelfter.Application.Features.Queries.Order.GetAll;
 using BookShelfter.Application.Features.Queries.Order.GetById;
 using BookShelfter.Application.Features.Queries.Order.GetOrderWithDetailsById;
 using MediatR;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BookShelfter.API.Controllers
+namespace BookShelfter.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize(AuthenticationSchemes = "Bearer")]
+public class OrdersController(IMediator mediator) : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class OrdersController : ControllerBase
+    private readonly IMediator _mediator = mediator;
+
+    [Authorize(Roles = "Admin")]
+    [HttpGet("GetAll")]
+    public async Task<IActionResult> GetAllOrders([FromQuery] GetAllOrdersQueryRequest request)
     {
-        IMediator _mediator;
-        public OrdersController(IMediator mediator)
-        {
-            _mediator = mediator;
-
-        }
-
-
-        [HttpGet("GetAll")]
-        public async Task<IActionResult> GetAllOrders([FromQuery] GetAllOrdersQueryRequest request)
-        {
-            var response = await _mediator.Send(request);
-            if (response.Success)
-                return Ok(response);
-
-
-            return BadRequest(response);
-
-        }
-
-
-        [HttpGet("GetOrderWithDetailsById/{orderId}")]
-        public async Task<IActionResult> GetOrderWithDetailsById([FromRoute] int orderId)
-        {
-            var request = new GetOrderWithDetailsByIdQueryRequest { OrderId = orderId };
-            var response = await _mediator.Send(request);
-
-            if (response.Success)
-            {
-                return Ok(response);
-            }
-
-            return BadRequest(response);
-        }
-
-
-        [HttpPost]
-        public async Task<IActionResult> CreateOrder([FromBody] CreateOrderCommandRequest request)
-        {
-            var response = await _mediator.Send(request);
-            if (response.Success)
-            {
-                return Ok(response);
-            }
-
-            return BadRequest(response);
-        }
-
-
-
-        [HttpPut("{orderId}")]
-        public async Task<IActionResult> UpdateOrder(int orderId, [FromBody] UpdateOrderCommandRequest request)
-        {
-            request.OrderId = orderId;
-            var response = await _mediator.Send(request);
-            if (response.Success)
-            {
-                return Ok(response.Message);
-            }
-
-            return BadRequest(response.Message);
-        }
-
-
-
-        [HttpDelete("{orderId}")]
-        public async Task<IActionResult> DeleteOrder(int orderId)
-        {
-            var request = new DeleteOrderCommandRequest { OrderId = orderId };
-            var response = await _mediator.Send(request);
-            if (response.Success)
-            {
-                return Ok(response.Message);
-            }
-
-            return BadRequest(response.Message);
-        }
-
-
-
-
-
-        [HttpGet("{orderId}")]
-        public async Task<IActionResult> GetOrderById(int orderId)
-        {
-            var request = new GetOrderByIdQueryRequest { OrderId = orderId };
-            var response = await _mediator.Send(request);
-
-            if (response.Success)
-            {
-                return Ok(response);
-            }
-
-            return BadRequest(response.Message);
-        }
+        var response = await _mediator.Send(request);
+        return response.Success
+            ? Ok(response)
+            : BadRequest(new { message = response.Message });
     }
 
-}
+    [Authorize(Roles = "Admin")]
+    [HttpGet("GetOrderWithDetailsById/{orderId}")]
+    public async Task<IActionResult> GetOrderWithDetailsById([FromRoute] int orderId)
+    {
+        var request = new GetOrderWithDetailsByIdQueryRequest { OrderId = orderId };
+        var response = await _mediator.Send(request);
+        return response.Success
+            ? Ok(response)
+            : BadRequest(new { message = response.Message });
+    }
 
+    [Authorize(Roles = "User")]
+    [HttpPost]
+    public async Task<IActionResult> CreateOrder([FromBody] CreateOrderCommandRequest request)
+    {
+        var response = await _mediator.Send(request);
+        return response.Success
+            ? Ok(response)
+            : BadRequest(new { message = response.Message });
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpPut("{orderId}")]
+    public async Task<IActionResult> UpdateOrder([FromRoute] int orderId, [FromBody] UpdateOrderCommandRequest request)
+    {
+        request.OrderId = orderId;
+        var response = await _mediator.Send(request);
+        return response.Success
+            ? Ok(new { message = response.Message })
+            : BadRequest(new { message = response.Message });
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpDelete("{orderId}")]
+    public async Task<IActionResult> DeleteOrder([FromRoute] int orderId)
+    {
+        var request = new DeleteOrderCommandRequest { OrderId = orderId };
+        var response = await _mediator.Send(request);
+        return response.Success
+            ? Ok(new { message = response.Message })
+            : BadRequest(new { message = response.Message });
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpGet("{orderId}")]
+    public async Task<IActionResult> GetOrderById([FromRoute] int orderId)
+    {
+        var request = new GetOrderByIdQueryRequest { OrderId = orderId };
+        var response = await _mediator.Send(request);
+        return response.Success
+            ? Ok(response)
+            : BadRequest(new { message = response.Message });
+    }
+}
